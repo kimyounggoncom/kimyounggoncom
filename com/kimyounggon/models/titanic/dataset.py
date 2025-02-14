3f804f765956526897771ba120515ff91640e115
kimyounggoncom <younggon0925@gmail.com>
2025-02-14 18:02:12 +0900

    타이타닉 시작 20250214 18:00

--- com/kimyounggon/models/titanic/dataset.py
+++ com/kimyounggon/models/titanic/dataset.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass 
-import pandas as pd  # pd = pandas()
 
 @dataclass  
 class Dataset:
--- com/kimyounggon/models/titanic/titanic_service.py
+++ com/kimyounggon/models/titanic/titanic_service.py
@@ -1,5 +1,25 @@
 from com.kimyounggon.models.titanic.dataset import Dataset
 import pandas as pd
+
+"""
+PassengerId  고객ID,
+Survived 생존여부,
+Pclass 승선권 1 = 1등석, 2 = 2등석, 3 = 3등석,
+Name,
+Sex,
+Age,
+SibSp 동반한 형제, 자매, 배우자,
+Parch 동반한 부모, 자식,
+Ticket 티켓번호,
+Fare 요금,
+Cabin 객실번호,
+Embarked 승선한 항구명 C = 쉐브루, Q = 퀸즈타운, S = 사우스햄튼
+print(f'결정트리 활용한 검증 정확도 {None}')
+print(f'랜덤포레스트 활용한 검증 정확도 {None}')
+print(f'나이브베이즈 활용한 검증 정확도 {None}')
+print(f'KNN 활용한 검증 정확도 {None}')
+print(f'SVM 활용한 검증 정확도 {None}')
+# """
 class TitanicService:
 
     dataset = Dataset()
@@ -13,5 +33,23 @@ class TitanicService:
 
 
 
+    @staticmethod
+    def pclass_ordinal():
+        pass
 
+    @staticmethod
+    def gender_ordinal():
+        pass
+
+    @staticmethod
+    def age_ordinal():
+        pass
+
+    @staticmethod
+    def fare_ordinal():
+        pass
+
+    @staticmethod
+    def Embarked_nominal():
+        pass
     
